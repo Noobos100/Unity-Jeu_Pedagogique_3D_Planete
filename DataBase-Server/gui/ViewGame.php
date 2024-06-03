cb7dccda431bc811a0cf862644b686757bd546a7
DeoxysTheGod <quentinb2004@gmail.com>
2024-06-03 14:01:37 +0200

    adding game

--- DataBase-Server/gui/ViewGame.php
+++ DataBase-Server/gui/ViewGame.php
@@ -6,50 +6,51 @@ use control\ControllerGameData;
 use data\DataAccess;
 
 include_once "View.php";
+
 class ViewGame extends View
 {
-    public function __construct(Layout $layout, ControllerGameData $controller, DataAccess $data)
-    {
-        parent::__construct($layout);
+	public function __construct(Layout $layout, ControllerGameData $controller, DataAccess $data)
+	{
+		parent::__construct($layout);
 
-        $parties = $controller->getParties($data);
+		$parties = $controller->getParties($data);
 
-        $this->title = 'Jeu';
+		$this->title = 'Jeu';
 		$this->content .= '<iframe src="/PlanetGame/index.html" width="800" height="600"></iframe>';
 
 
 		// Add the button
-        $this->content .= '<button id="popupButton">Scores</button>';
+		$this->content .= '<button id="popupButton">Scores</button>';
 
-        // Add the popup with "Hello World" content
-        $this->content .= '
+		// Add the popup with "Hello World" content
+		$this->content .= '
             <div id="popup" class="popup2">
                 <div class="popup-content">
                             <button id="closeButton" >X</button>
                     <h1>Top score</h1>';
-        // Afficher le temps minimum
-        $totalMinTemps = $controller->calculateTempsMin($parties);
-        $this->content .= "<p>Temps minimum de jeu : $totalMinTemps</p>";
+		// Afficher le temps minimum
+		$totalMinTemps = $controller->calculateTempsMin($parties);
+		$this->content .= "<p>Temps minimum de jeu : $totalMinTemps</p>";
 
-        // Afficher le meilleur score
-        $bestUsers = $controller->getBestUsers($data);
-        $this->content .= '<h2>Meilleurs scores</h2>';
-        $this->content .= '<table>';
-        $this->content .= '<tr><th>Utilisateur</th><th>Score</th></tr>';
-        foreach ($bestUsers as $user) {
-            $this->content .= '<tr>';
-            $this->content .= '<td>' . $user['Username'] . '</td>';
-            $this->content .= '<td>' . $user['Max_Score'] . '</td>';
-            $this->content .= '</tr>';
-        }
+		// Afficher le meilleur score
+		$bestUsers = $controller->getBestUsers($data);
+		$this->content .= '<h2>Meilleurs scores</h2>';
+		$this->content .= '<table>';
+		$this->content .= '<tr><th>Utilisateur</th><th>Score</th></tr>';
+		foreach ($bestUsers as $user) {
+			$this->content .= '<tr>';
+			$this->content .= '<td>' . $user['Username'] . '</td>';
+			$this->content .= '<td>' . $user['Max_Score'] . '</td>';
+			$this->content .= '</tr>';
+		}
 
 
-        $this->content .= '
+		$this->content .= '
                 </div>
             </div>
         ';
 
-        $this->content .= '
+		$this->content .= '
 <script>
     document.getElementById(\'popupButton\').addEventListener(\'click\', function() {
         var popup = document.getElementById(\'popup\');
@@ -62,7 +63,7 @@ class ViewGame extends View
 </script>
 ';
 
-        $this->content .= '
+		$this->content .= '
 			<script>
 				document.getElementById(\'closeButton\').addEventListener(\'click\', function() {
 					var popup = document.getElementById(\'popup\');
