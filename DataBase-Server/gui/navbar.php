4d8810e17eba07171ed7f54b763ce6d9ba58d978
Noobos100 <charles.daw@etu.univ-amu.fr>
2024-06-04 16:55:20 +0200

    Ajoute manuel d'utilisation du jeu dans la navbar

--- DataBase-Server/gui/navbar.php
+++ DataBase-Server/gui/navbar.php
@@ -7,9 +7,10 @@ if (isset($_SESSION['loggedin'])) {
         <button class="sidebar-button" onclick="location.href=\'/game-data\'">Données de jeu</button>
         <button class="sidebar-button" onclick="location.href=\'/players\'">Joueurs</button>
         <button class="sidebar-button" onclick="location.href=\'/logout\'">Déconnexion</button>';
-
+// ajouter bouton pour ouvrir le manuel d'utilisation du site une fois qu'il sera terminé
 } else {
     return ' 
         <button class="sidebar-button" onclick="location.href=\'/game\'">Jeu</button>
-        <button class="sidebar-button" onclick="location.href=\'/login\'">Connexion</button>';
+        <button class="sidebar-button" onclick="location.href=\'/login\'">Connexion</button>
+        <button class="sidebar-button" onclick="window.open(\'/assets/manual/Manuel_dutilisation_windows.pdf\', \'_blank\')">Manuel d\'utilisation</button>';
 }
