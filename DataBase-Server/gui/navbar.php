f7ce9418aa316baec84246eb187d273772e42d5b
Noobos100 <charles.daw@etu.univ-amu.fr>
2024-06-06 16:16:13 +0200

    Ajoute liens vers manuels

--- DataBase-Server/gui/navbar.php
+++ DataBase-Server/gui/navbar.php
@@ -6,10 +6,12 @@ if (isset($_SESSION['loggedin'])) {
         <button class="sidebar-button" onclick="location.href=\'/manage-questions\'">Questions</button>
         <button class="sidebar-button" onclick="location.href=\'/game-data\'">Données de jeu</button>
         <button class="sidebar-button" onclick="location.href=\'/players\'">Joueurs</button>
+        <button class="sidebar-button" onclick="window.open(\'https://jeupedagogique.pq.lu/manuals/Manuel_dutilisation_dashboard.pdf\', \'_blank\')">Manuel du jeu</button>
         <button class="sidebar-button" onclick="location.href=\'/logout\'">Déconnexion</button>';
-// ajouter bouton pour ouvrir le manuel d'utilisation du site une fois qu'il sera terminé
+
 } else {
     return ' 
         <button class="sidebar-button" onclick="location.href=\'/game\'">Voyage autour du Soleil</button>
-        <button class="sidebar-button" onclick="location.href=\'/login\'">Connexion</button>';
+        <button class="sidebar-button" onclick="location.href=\'/login\'">Connexion</button>
+        <button class="sidebar-button" onclick="window.open(\'https://jeupedagogique.pq.lu/manuals/Manuel_dutilisation_windows.pdf\', \'_blank\')">Manuel du dashboard</button>';
 }
