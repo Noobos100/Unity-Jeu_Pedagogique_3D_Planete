37ad1be6ec5a1509ce45f7271a73bbccd7c4dd4b
Noobos100 <charles.daw@etu.univ-amu.fr>
2024-06-06 17:12:03 +0200

    fix typo

--- DataBase-Server/gui/navbar.php
+++ DataBase-Server/gui/navbar.php
@@ -6,12 +6,12 @@ if (isset($_SESSION['loggedin'])) {
         <button class="sidebar-button" onclick="location.href=\'/manage-questions\'">Questions</button>
         <button class="sidebar-button" onclick="location.href=\'/game-data\'">Données de jeu</button>
         <button class="sidebar-button" onclick="location.href=\'/players\'">Joueurs</button>
-        <button class="sidebar-button" onclick="window.open(\'https://jeupedagogique.pq.lu/manuals/Manuel_dutilisation_dashboard.pdf\', \'_blank\')">Manuel du jeu</button>
+        <button class="sidebar-button" onclick="window.open(\'https://jeupedagogique.pq.lu/manuals/Manuel_dutilisation_dashboard.pdf\', \'_blank\')">Manuel du dashboard</button>
         <button class="sidebar-button" onclick="location.href=\'/logout\'">Déconnexion</button>';
 
 } else {
     return ' 
         <button class="sidebar-button" onclick="location.href=\'/game\'">Voyage autour du Soleil</button>
         <button class="sidebar-button" onclick="location.href=\'/login\'">Connexion</button>
-        <button class="sidebar-button" onclick="window.open(\'https://jeupedagogique.pq.lu/manuals/Manuel_dutilisation_windows.pdf\', \'_blank\')">Manuel du dashboard</button>';
+        <button class="sidebar-button" onclick="window.open(\'https://jeupedagogique.pq.lu/manuals/Manuel_dutilisation_windows.pdf\', \'_blank\')">Manuel du jeu</button>';
 }
